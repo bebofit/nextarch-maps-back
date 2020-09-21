f35f03c16949e9c861c8fa5dccbac79006bd2e2d
Abdelrahman Soliman <bebofit@aucegypt.edu>
2020-09-21 16:11:14 +0200

    sf

--- src/api/admins/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import errorHandler from 'express-async-handler';
-import { isAuthenticated, isNotAuthenticated } from '../../common/middleware';
-// import * as controller from './controller';
-
-const router = Router();
-
-export default router;
--- src/lib/sendgrid.ts
+++
@@ -1,8 +0,0 @@
-import sgMail from '@sendgrid/mail';
-import config from '../config';
-
-const { SENDGRID_API } = config;
-
-sgMail.setApiKey(SENDGRID_API);
-
-export default sgMail;
