957865618410869a07636523d156bb6679a53161
Abdelrahman Soliman <bebofit@aucegypt.edu>
2020-11-19 18:44:44 +0200

    add img api

--- src/common/services/notification.ts
+++ src/common/services/notification.ts
@@ -1,53 +1,53 @@
-import admin from '../../lib/firbase-admin';
+// import admin from '../../lib/firbase-admin';
 
-const sendMsg = async (title: string, body: string, topic = 'general') => {
-  try {
-    const response = await admin.messaging().sendToTopic(topic, {
-      notification: {
-        title,
-        body
-      }
-    });
-    console.log('Successfully sent message:', response);
-  } catch (error) {
-    console.log('Error sending message:', error);
-  }
-};
+// const sendMsg = async (title: string, body: string, topic = 'general') => {
+//   try {
+//     const response = await admin.messaging().sendToTopic(topic, {
+//       notification: {
+//         title,
+//         body
+//       }
+//     });
+//     console.log('Successfully sent message:', response);
+//   } catch (error) {
+//     console.log('Error sending message:', error);
+//   }
+// };
 
-const sentToDevice = async (
-  registrationToken: string,
-  title: string,
-  body: string
-) => {
-  try {
-    const response = await admin.messaging().sendToDevice(
-      registrationToken,
-      {
-        notification: {
-          title,
-          body
-        }
-      },
-      {
-        priority: 'high',
-        timeToLive: 60 * 60 * 24
-      }
-    );
-    console.log('Successfully sent message:', response);
-  } catch (error) {
-    console.log('Error sending message:', error);
-  }
-};
+// const sentToDevice = async (
+//   registrationToken: string,
+//   title: string,
+//   body: string
+// ) => {
+//   try {
+//     const response = await admin.messaging().sendToDevice(
+//       registrationToken,
+//       {
+//         notification: {
+//           title,
+//           body
+//         }
+//       },
+//       {
+//         priority: 'high',
+//         timeToLive: 60 * 60 * 24
+//       }
+//     );
+//     console.log('Successfully sent message:', response);
+//   } catch (error) {
+//     console.log('Error sending message:', error);
+//   }
+// };
 
-const registerUser = async (registrationToken: string) => {
-  try {
-    const response = await admin
-      .messaging()
-      .subscribeToTopic(registrationToken, 'general');
-    console.log('Successfully registered:', response);
-  } catch (error) {
-    console.log('Error registering:', error);
-  }
-};
+// const registerUser = async (registrationToken: string) => {
+//   try {
+//     const response = await admin
+//       .messaging()
+//       .subscribeToTopic(registrationToken, 'general');
+//     console.log('Successfully registered:', response);
+//   } catch (error) {
+//     console.log('Error registering:', error);
+//   }
+// };
 
-export { sendMsg, sentToDevice, registerUser };
+// export { sendMsg, sentToDevice, registerUser };
